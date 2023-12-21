98765f7e3da9275657cc935322dcaa61178646bc
buse-serinol <buserinoll@gmail.com>
2023-12-21 23:34:52 +0300

    Added addItem function to add items to to-do list

---
+++ main.js
@@ -0,0 +1,44 @@
+// html elementleri
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.querySelector("#grocery"); // input değeri
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+let editID = "";
+let editElement; //düzenleme yapılan öğeyi temsil eder
+let editFlag = false; // düzenleme modunda olup olmadığını belirtir.
+
+// form gönderildiğinde addItem fonk. çağır.
+
+form.addEventListener("submit", addItem);
+
+//! functions
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value; // input giriş değerini al
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attribute = document.createAttribute("data-id"); // yeni bir veri kimliği oluşturur
+    attribute.value = id;
+    element.setAttributeNode(attribute);
+    element.classList.add("grocery-item");
+
+    element.innerHTML = `
+                <p class="tittle">${value}</p>
+                <div class="btn-container">
+                    <button class="edit-btn" type="button">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button class="delete-btn" type="button">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>`;
+    list.appendChild(element);
+    grocery.value = "";
+  }
+}
